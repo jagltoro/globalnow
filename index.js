3c99fbe43154151231daabc16e9bb23eae8a5152
Jesus Gonzalez <jagltoro@gmail.com>
2021-04-24 13:03:28 -0300

    Start parsing the data from the page

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const axios = require('axios');
+const { JSDOM } = require('jsdom');
 
 const url = 'https://www.apple.com';
-const searchParam = 'reimagined';
+const searchParam = 'government';
+
+const regexp = new RegExp(searchParam, "g");
 
 async function fetchData(url){
   console.log(`Getting data from the URL ${url}`);
   let response = await axios(url).catch((err) => console.log(err));
   if(response.status !== 200){
       console.err("Cannot get the data from the specified URL");
+      return;
   }
+  return response.data;
+}
 
-  console.log(response);
+function parseData(data){
+  const dom = new JSDOM(data);
+  const anchorsList = [...dom.window.document.querySelectorAll('a')];
+  
+  
+  console.log(nodeList)
 }
 
 
 
-fetchData(url);
+fetchData(url).then((HTMLOutput) => {
+  if(HTMLOutput){
+    parseData(HTMLOutput);
+  }
+});
